4801efde93eb9e38dffbddfebd99f0e650a1af6c
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-09-29 13:42:14 +0100

    decimalToHex function for correct color coding

--- blinkstick.js
+++ blinkstick.js
@@ -314,7 +314,16 @@ BlinkStick.prototype.getColor = function (callback) {
 };
 
 
+function decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
 
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
 
 /**
  * Get the current color settings as hex string.
