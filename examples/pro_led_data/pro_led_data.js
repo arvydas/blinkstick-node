74641ced6038b3e7cfd6128e14d94a7f4f125fcf
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-09-29 14:18:00 +0100

    Added example to get LED data frame

---
+++ examples/pro_led_data/pro_led_data.js
@@ -0,0 +1,14 @@
+var blinkstick = require('blinkstick'),
+  device = blinkstick.findFirst();
+
+if (device) {
+  var finished = false;
+
+  device.getColors(64, function(err, data) {
+    console.log(data);
+    finished = true;
+  });
+
+  var wait = function () { if (!finished) setTimeout(wait, 100)}
+  wait();
+}
