f2eef3ef9abfec41475f5e621c8b18be42536fbe
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-09-29 13:44:08 +0100

    Updated info example with more details and wait to finish

--- examples/info/info.js
+++ examples/info/info.js
@@ -2,16 +2,30 @@ var blinkstick = require('blinkstick'),
   device = blinkstick.findFirst();
 
 if (device) {
+  var finished = false;
+
+  console.log("Device found!");
   device.getDescription(function(error, result) {
-    console.log("Description: " + result);
+    console.log("Description:  " + result);
 
     device.getManufacturer(function(error, result) {
       console.log("Manufacturer: " + result);
 
       device.getSerial(function(error, result) {
-        console.log("Serial: " + result);
-        console.log("Requires software color patch: " + device.requiresSoftwareColorPatch);
+        console.log("Serial:       " + result);
+
+        device.getMode(function(error, result) {
+          console.log("Mode:         " + result);
+
+          device.getColorString(function(error, result) {
+            console.log("Color:        " + result);
+            finished = true;
+          });
+        });
       });
     });
   });
+
+  var wait = function () { if (!finished) setTimeout(wait, 100)}
+  wait();
 }
