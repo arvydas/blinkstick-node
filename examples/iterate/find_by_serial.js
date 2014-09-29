4a005f4e325238f52aa8c048249b3ce3fe4ed5b6
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-09-29 22:30:23 +0100

    Examples to search for BlinkSticks

---
+++ examples/iterate/find_by_serial.js
@@ -0,0 +1,9 @@
+var blinkstick = require('blinkstick');
+
+blinkstick.findBySerial("BS000000-1.0", function(bstick) {
+    if (bstick) {
+        console.log("BlinkStick found!");
+    } else {
+        console.log("BlinkStick not found...");
+    }
+});
---
+++ examples/iterate/find_serials.js
@@ -0,0 +1,5 @@
+var blinkstick = require('blinkstick');
+
+blinkstick.findAllSerials(function(serials) {
+    console.log(serials);
+});
