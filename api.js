e4309fbf397700984f44bdf80da8b9a8be4a69de
Arvydas Juskevicius <arvydas@agileinnovative.co.uk>
2014-09-30 13:11:35 +0100

    Updated Release 1.0.0

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "BlinkStick",
-        "blinkstick"
+        "ModuleExports"
     ],
     "modules": [
         "blinkstick"
