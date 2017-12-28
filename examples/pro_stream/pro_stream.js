3f330f6b721c47304fac6a5fc1e789cccab88e5c
dcerisano <dcerisano@standard3d.com>
2017-12-28 01:34:04 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/pro_stream/pro_stream.js
+++ examples/pro_stream/pro_stream.js
@@ -1,85 +1,33 @@
-//Stream producer-consumer pattern allows separation of concerns for BlinkStick animations.
-//Producer pushes frames to the stream as simple RGB arrays at a fixed rate.
+//Stream producer-consumer pattern than allows separation of concerns for BlinkStick frame streaming.
+//Producer pushes frames to the stream as simple RGB arrays at a variable frame rate.
 //Consumer pulls frames from the stream and sends them to BlickStick at the same rate.
 //This is elastic and very efficient, requiring negligible CPU for the node process. 
-//TODO: Johnny Five integration for BlinkStick - this pattern could form the basis.
+
 
 var blinkstick = require('blinkstick');
 var device     = blinkstick.findFirst();
 
+//Variable frame rate in frames per second (fps)
+//BlinkStick can handle maximum 60fps
+var framerate = 60;
+
 //Stream buffer for frames
 var stream_buffer = [];
 
-//Stream buffer length should always only be 1 if the producer and consumer can run at the same rate.
-//This provides for some buffering to avoid skipped frames.
-var MAX_BUFFER_LENGTH = 100;
-
-//Streaming rates in milliseconds
-//Eg. 16ms is 60fps. 20ms is recommended minimum for BlinkStick, but 16ms seems perfectly stable.
-//Producer rate should never be faster than consumer rate (or frames will be skipped).
-var rate = 80; // 80 is nice for this particular animation 
-
-
-//Image of 8 LEDS to be animated (this one is Knight Rider. Or a Cylon)
-var image = [
-	000, 000, 000,
-	000, 000, 000,
-	000, 000, 000,
-	000, 000, 000,
-	000, 000, 000,
-	008, 000, 000, //Iris
-	255, 016, 000, //Pupil
-	008, 000, 000  //Iris
-	];
-
-//Animation variables
-var phase = 0;
-var shift = 0;
-var dir   = 1;
-
+//This provides some elasticity to avoid skipped frames.
+var MAX_BUFFER_LENGTH = 60;
 
 //Stream Producer 
-//Shifts the image back and forth.
-//Pushes the resulting frame to the stream (skips it if stream buffer is full)
 function producer(){
 
-	phase +=dir;
-	if (phase>5)
-	{
-		phase=5;
-		dir=-dir;
-	}
-
-	if (phase<0){
-		phase=0;
-		dir=-dir;
-	}
-
-	shift = Math.floor(phase);
-	frame = new Array(8*3).fill(0);
-
-	for (var i = 0; i< 8; i++) {
-		
-		//Convert to BlinkStick RGB format (GRB)
-		frame[i*3+1] = image[shift*3+0]; // R
-		frame[i*3+0] = image[shift*3+1]; // G
-		frame[i*3+2] = image[shift*3+2]; // B
-
-		shift+=1;
-		if (shift>=8)
-			shift=0;
-	}
-
 	if (stream_buffer.length<MAX_BUFFER_LENGTH)
-		stream_buffer.push(frame);
+		stream_buffer.push(onFrame());
 
-	setTimeout(producer, rate);
+	framerate = Math.max(1, Math.min(framerate, 60));
+	setTimeout(producer, 1000/framerate);
 }
 
-
 //Stream Consumer
-//Gets the next frame (if any) from the stream.
-//Sends the frame (if any) to the BlinkStick.
 function consumer()
 {
 	if (stream_buffer.length>0)
@@ -87,16 +35,62 @@ function consumer()
 		var next = stream_buffer.shift();
 		device.setColors(0, next, function(err, next) {});
 	}
-	setTimeout(consumer, rate);
+	
+	framerate = Math.max(1, Math.min(framerate, 60));
+	setTimeout(consumer, 1000/framerate);
 }
-
 
 //Start streaming
-
 if (device){
-	setTimeout(producer, rate);
+	producer();
+	consumer();
 }
 
-if (device){
-	setTimeout(consumer, rate);
+
+
+//User defined frame generator.
+//Returns the next frame.
+//Called by the producer.
+//Pixel source can be anything (eg. a running screenshot scaled to 8x1 resolution for ambient display applications)
+//This example is an animation that shifts an image of an eye back and forth.
+
+function onFrame()
+{       
+	var frame = [];
+	framerate = 15;  // 15fps (can be varied by this function but clamped between 1 and 60 in the stream)
+
+	if (phase<0 || phase>5)
+		speed=-speed;
+
+	phase += speed;
+	shift = Math.floor(phase);
+
+	for (var i = 0; i<size; i++) {
+		//Convert to BlinkStick RGB format (GRB)
+		frame[i*3+1] = pixels[shift*3+0]; // R
+		frame[i*3+0] = pixels[shift*3+1]; // G
+		frame[i*3+2] = pixels[shift*3+2]; // B
+
+		if (shift<size)
+			shift+=1;
+	}
+	return frame;
 }
+
+//Pixel source (this one is Knight Rider, or a Cylon)
+var pixels = [
+	000, 000, 000,
+	000, 000, 000,
+	000, 000, 000,
+	000, 000, 000,
+	000, 000, 000,
+	008, 000, 000, //Iris
+	128, 016, 000, //Pupil
+	008, 000, 000  //Iris
+	];
+
+//Animation variables
+var phase = 0;
+var shift = 0;
+var speed = 1;
+var size  = pixels.length/3;
