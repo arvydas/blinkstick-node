a382d640e932fda35bccc3e43a3e41c9845e3f29
dcerisano <dcerisano@standard3d.com>
2018-01-02 18:01:56 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/ambient-display/ambient-display.js
+++ examples/ambient-display/ambient-display.js
@@ -1,5 +1,6 @@
 //Ambient display based on pro_stream.js example     
-//Realtime streaming of desktop to blinkstick                                                                                                                                                           
+//Realtime streaming of desktop to blinkstick  
+//
 //Minimum Requirements: 
 // - Latest version of nodejs (tested with v8.9.3)
 // - 4 core 3GHz processor, 8MB ram, and 2MB graphics card
