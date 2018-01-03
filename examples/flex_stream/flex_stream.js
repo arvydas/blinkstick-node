56173704abe9b505e1d479afcffeed7d1fe8fb05
dcerisano <dcerisano@standard3d.com>
2018-01-03 01:29:02 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/flex_stream.js
+++ examples/flex_stream/flex_stream.js
@@ -127,7 +127,7 @@ function morphFrame(grb)
 	if (backingstore == null || transparency == 0)
 		backingstore = grb;
 	
-    //Morph the new frame with current backingstore (additive colour blending function)
+    //Morph the new frame with current backingstore (additive alpha colour blending)
 	if (transparency>0){   
 		for (var i = 0; i<getSize(); i++) {
 			backingstore[i*3+0] = Math.floor(backingstore[i*3+0]*transparency + grb[i*3+0]*(1-transparency)); // R
