99d2cbec190e287c8900e8e93875066ac96c9b9a
dcerisano <dcerisano@standard3d.com>
2018-01-03 18:58:19 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

---
+++ examples/flex_stream/fireplace.js
@@ -0,0 +1,41 @@
+//Fireplace ambience based on flex_stream.js
+//For Windows, Linux and Mac
+
+const os          = require("os");
+const flex_stream = require("./flex_stream.js");
+
+var size               = 8;   // Default 8, maximum 64 (single BlickStick channel)
+var producer_framerate = 30;  // Varies for flame flickering
+var consumer_framerate = 60;  // High fps for morphing   
+var alpha              = 0;   // variable opacity for flickering flames
+
+var pos           = 1;
+var speed         = 1;
+
+function onFrame() {       
+        var frame = flex_stream.newFrame();
+        
+        for (i=0; i<size; i++)
+        {
+                //Red to yellow spectrum
+                var r = Math.random()*255;
+                var g = r*Math.random()*.5;
+             
+                frame[i*3+0] = Math.floor(r);  //R
+                frame[i*3+1] = Math.floor(g);  //G
+                frame[i*3+2] = 0;              //B
+
+                //Flickering frames
+                f = Math.random();
+                flex_stream.setProducerFramerate(f*8+5);
+                flex_stream.setAlpha(.1+(f/2));
+        }
+        flex_stream.produceFrame(frame);     
+}
+
+//Configure stream
+flex_stream.setSize(size);
+flex_stream.setProducerFramerate(producer_framerate);
+flex_stream.setConsumerFramerate(consumer_framerate);
+flex_stream.setAlpha(alpha);
+flex_stream.setOnFrame(onFrame);
