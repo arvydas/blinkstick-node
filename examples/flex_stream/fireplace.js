55eef9c087ee48a042d80847d047e83b078f8171
dcerisano <dcerisano@standard3d.com>
2018-01-03 20:32:20 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/fireplace.js
+++ examples/flex_stream/fireplace.js
@@ -25,7 +25,7 @@ function onFrame() {
 		}
 		//Flickering frames
 		f = Math.random();
-		flex_stream.setProducerFramerate(f*8+4);
+		flex_stream.setProducerFramerate(f*10+2);
 		flex_stream.setAlpha(.1+(f/10));
 	}
 	flex_stream.produceFrame(frame);     
