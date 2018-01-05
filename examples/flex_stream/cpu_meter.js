a8f5dc9e29ba19fe774dd4aaacbc5cca531f4865
dcerisano <dcerisano@standard3d.com>
2018-01-04 19:34:43 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/cpu_meter.js
+++ examples/flex_stream/cpu_meter.js
@@ -34,7 +34,7 @@ function onFrame() {
 		speed =-speed;         
 	pos += speed;
 	
-	clearFrame(frame);
+	flex_stream.clearFrame(frame);
 	//Vary particle colour by CPU load (green to amber to red)        
 	frame[pos*3+0] = Math.floor(cpu_avg*2.5)+5; //R
 	frame[pos*3+1] = 100-Math.floor(cpu_avg);   //G
