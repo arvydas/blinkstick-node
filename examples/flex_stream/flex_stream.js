5e8805170849d3520871202b478d2bbc03e6508a
dcerisano <dcerisano@standard3d.com>
2018-01-04 23:37:07 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/flex_stream.js
+++ examples/flex_stream/flex_stream.js
@@ -238,7 +238,7 @@ if (device){
 var pos = 0;
 var signatureOnFrame = function(){
 	//Bounce particle off edges of LED strip
-	if (pos++ >= size*2)
+	if (pos++ >= size+30)
 		pos=0;       
 
 	var frame = newFrame();
