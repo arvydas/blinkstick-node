44302f2cd2276ebdc663cde602cb10de1dc5fdae
dcerisano <dcerisano@standard3d.com>
2018-01-05 20:49:18 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/ambilight.js
+++ examples/flex_stream/ambilight.js
@@ -10,9 +10,9 @@ const  flex_stream = require("./flex_stream.js");
 const  screenshot  = require('screenshot-desktop'); //Available at npmjs.com                                                                                                                                       
 const  sharp       = require('sharp');              //Available at npmjs.com                                                                                                                                             
 
-var size               = 8;   // Default 8, maximum 64 (single BlickStick channel)
-var producer_framerate = 10;   // Low sample rate (5 fps = 200ms lag) to reduce CPU overhead 
-var consumer_framerate = 60;  // High render rate for smooth morphing/interlacing
+var size               = 8;  // Default 8, maximum 64 (single BlickStick channel)
+var producer_framerate = 10; // Low sample rate (10 fps = 100ms lag) to reduce CPU overhead 
+var consumer_framerate = 60; // High render rate for smooth morphing/interlacing
 var alpha              = 0.2 // 15% opacity for smooth morphing/interlacing
 
 //Stream scaled desktop (size x 1) to BlinkStick via async futures pipeline
