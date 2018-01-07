9ae2c654d7200026e70670bd059cc5068e2ce88e
dcerisano <dcerisano@standard3d.com>
2018-01-07 04:26:37 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/aurora.js
+++ examples/flex_stream/aurora.js
@@ -10,24 +10,28 @@ module.exports = {
 const os          = require("os");
 const flex_stream = require("./flex_stream.js");
 
-
+	
 var frame = flex_stream.newFrame();
 function aurora() {       
 	//Aurora
 	a = Math.random();
-
+	flex_stream.setProducerFramerate(a*2+1);
+	flex_stream.setAlpha(.001+(a/100));
+	
+    //Borealis
 	for (i=0; i<flex_stream.getSize(); i++)
 	{    			
 		var r = Math.random()*50;
 		var g = (1-Math.random()*.85)*50;
 		var b = (1-Math.random()*.85)*100;   
-		//Borealis
-		if (Math.random()>.99)
+		
+		if (Math.random()>.5)
 		{
-			frame[i*3+0] = Math.floor(r);  //R
-			frame[i*3+1] = Math.floor(g);  //G
-			frame[i*3+2] = Math.floor(b);  //B
+			r=0; g=0; b=0;
 		}
+		frame[i*3+0] = Math.floor(r);  //R
+		frame[i*3+1] = Math.floor(g);  //G
+		frame[i*3+2] = Math.floor(b);  //B
 	}
 	flex_stream.produceFrame(frame);     
 }
@@ -36,9 +40,9 @@ function aurora() {
 
 function init(){
 	flex_stream.setSize(8);
-	flex_stream.setProducerFramerate(5);
+	flex_stream.setProducerFramerate(60);
 	flex_stream.setConsumerFramerate(60);
-	flex_stream.setAlpha(0.00001);
+	flex_stream.setAlpha(1);
 	flex_stream.setOnFrame(aurora);
 }
 
