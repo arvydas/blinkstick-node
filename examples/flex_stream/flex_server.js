615ad6a4636ff974e431e06fcfbdfcd9378f9139
dcerisano <dcerisano@standard3d.com>
2018-01-06 23:34:02 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/flex_server.js
+++ examples/flex_stream/flex_server.js
@@ -1,13 +1,16 @@
 //Very simple server to switch between examples
-// http://localhost:5000/?example=name
+//eg. curl http://localhost:5000/?example=name
+//Last included example (eg. cpu_meter) is started.
+//Default is the flex stream signature
 
+const express     = require('express');
 const flex_stream = require("./flex_stream.js");
 const ambilight   = require("./ambilight.js");
 const unicorn     = require("./unicorn.js");
 const fireplace   = require("./fireplace.js");
 const aurora      = require("./aurora.js");
 const cpu_meter   = require("./cpu_meter.js");
-const express     = require('express');
+
 
 var app = express()
 
