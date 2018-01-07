3e4e55e035de0847e3407aaf09b5c97b27ecae84
dcerisano <dcerisano@standard3d.com>
2018-01-06 21:51:53 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/flex_server.js
+++ examples/flex_stream/flex_server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 
-app.get('/', function (req, res) {
+app.get('/p/:onFrame', function (req, res) {
 	res.send("onFrame is set to " + req.params.onFrame);
 })
 
