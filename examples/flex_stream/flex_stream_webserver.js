b55c4daeb71191074d3c9856cd65aeef85e6b357
dcerisano <dcerisano@standard3d.com>
2018-01-07 18:08:39 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/flex_stream_webserver.js
+++ examples/flex_stream/flex_stream_webserver.js
@@ -13,7 +13,7 @@ const cpu_meter   = require("./cpu_meter.js"); //This starts first
 
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
 	var example = req.query.example;
 
 	switch(example) {
