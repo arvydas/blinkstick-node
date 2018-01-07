8f155aeb5185ded7ff54797ffbf5e26e08c341e8
dcerisano <dcerisano@standard3d.com>
2018-01-07 17:17:35 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

---
+++ examples/flex_stream/timesquare.js
@@ -0,0 +1,43 @@
+//Times Square ambience based on flex_stream.js
+//For Windows, Linux and Mac
+
+module.exports = {
+		init: function() {
+			init(); 
+		}
+}
+
+const os          = require("os");
+const flex_stream = require("./flex_stream.js");
+
+var frame = flex_stream.newFrame();
+
+function unicorn() {       
+	// Unicorn rainbow happy joy 
+	var off = 0;
+	var amp = 150;
+
+	frame.shift();
+
+	var r = Math.random()*amp+off;
+	var g = Math.random()*amp+off;
+	var b = Math.random()*amp+off;
+
+	frame.push(Math.floor(r));  //R
+	frame.push(Math.floor(g));  //G
+	frame.push(Math.floor(b));  //B
+
+	flex_stream.produceFrame(frame);     
+}
+
+//Configure stream
+
+function init(){
+	flex_stream.setSize(8);
+	flex_stream.setProducerFramerate(4);
+	flex_stream.setConsumerFramerate(60);
+	flex_stream.setAlpha(0.05);
+	flex_stream.setOnFrame(unicorn);
+}
+
+init();
