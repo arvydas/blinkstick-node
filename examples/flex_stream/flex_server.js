4b091c142c7d20ecf3aebe62143a861562dc9203
dcerisano <dcerisano@standard3d.com>
2018-01-06 21:49:58 -0500

    Signed-off-by: dcerisano <dcerisano@standard3d.com>

--- examples/flex_stream/flex_server.js
+++ examples/flex_stream/flex_server.js
@@ -1,15 +1,11 @@
-//Utilities for flex stream
-
-
 var express = require('express')
 var app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
+
 app.get('/', function (req, res) {
-  res.send('hello world')
+	res.send("onFrame is set to " + req.params.onFrame);
 })
 
 var port = process.env.PORT || 5000;
  app.listen(port, function() {
-   console.log("Listening on " + port);
  });
